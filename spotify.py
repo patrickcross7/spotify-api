fda8f739b6098e82f5620199809e99211ff90a72
patrickcross7 <patrickcross9@gmail.com>
2023-07-04 17:13:24 -0400

    New changes to spotify and new function

--- spotify.py
+++ spotify.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post, get
 import json
 
 load_dotenv()
@@ -9,7 +9,7 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret)
+#print(client_id, client_secret)
 
 def get_token():
     auth_string = client_id + ":" + client_secret
@@ -29,7 +29,41 @@ def get_token():
     token = json_result["access_token"]
     return token
     
-token = get_token()
-print(token)
 
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"q={artist_name}&type=artist&limit=1"
+
+    query_url = url + "?" + query
+
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
     
+    if(len(json_result) == 0):
+        print("No artist with that name exists")
+        return None
+    return json_result[0]
+
+    # print(json_result)
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+token = get_token()
+artist = input("What artist would you like to search for: ")
+result = search_for_artist(token, artist)
+artist_id = result["id"]
+print("Here are the top ten songs from " + artist + "!")
+songs = get_songs_by_artist(token, artist_id)
+
+for i, song in enumerate(songs):
+    print(f"{i + 1}. {song['name']}")
